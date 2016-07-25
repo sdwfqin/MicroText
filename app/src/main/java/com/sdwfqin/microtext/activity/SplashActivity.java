339401db7294a94ed1ffc1110dd150e0355c6f54
sdwfqin <1245366907@qq.com>
2016-07-25 14:59:12 +0800

    首页广告

--- app/src/main/java/com/sdwfqin/microtext/activity/SplashActivity.java
+++ app/src/main/java/com/sdwfqin/microtext/activity/SplashActivity.java
@@ -1,13 +1,12 @@
 package com.sdwfqin.microtext.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
+import android.widget.RelativeLayout;
 
+import com.baidu.mobads.SplashAd;
+import com.baidu.mobads.SplashAdListener;
 import com.sdwfqin.microtext.R;
 import com.sdwfqin.microtext.base.BaseActivity;
 
@@ -16,7 +15,90 @@ import com.sdwfqin.microtext.base.BaseActivity;
  */
 public class SplashActivity extends BaseActivity {
 
-    private static final long DELAY = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // adUnitContainer
+        RelativeLayout adsParent = (RelativeLayout) this.findViewById(R.id.adsRl);
+
+        // the observer of AD
+        SplashAdListener listener = new SplashAdListener() {
+            @Override
+            public void onAdDismissed() {
+                Log.i("RSplashActivity", "onAdDismissed");
+                jumpWhenCanClick(); // 跳转至您的应用主界面
+            }
+
+            @Override
+            public void onAdFailed(String arg0) {
+                Log.i("RSplashActivity", "onAdFailed");
+                jump();
+            }
+
+            @Override
+            public void onAdPresent() {
+                Log.i("RSplashActivity", "onAdPresent");
+            }
+
+            @Override
+            public void onAdClick() {
+                Log.i("RSplashActivity", "onAdClick");
+                // 设置开屏可接受点击时，该回调可用
+            }
+        };
+        String adPlaceId = "bf827312"; // 重要：请填上您的广告位ID，代码位错误会导致无法请求到广告
+        new SplashAd(this, adsParent, listener, adPlaceId, true);
+    }
+
+    /**
+     * 当设置开屏可点击时，需要等待跳转页面关闭后，再切换至您的主窗口。故此时需要增加canJumpImmediately判断。 另外，点击开屏还需要在onResume中调用jumpWhenCanClick接口。
+     */
+    public boolean canJumpImmediately = false;
+
+    private void jumpWhenCanClick() {
+        Log.d("test", "this.hasWindowFocus():" + this.hasWindowFocus());
+        if (canJumpImmediately) {
+            enterHome();
+        } else {
+            canJumpImmediately = true;
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        canJumpImmediately = false;
+    }
+
+    /**
+     * 不可点击的开屏，使用该jump方法，而不是用jumpWhenCanClick
+     */
+    private void jump() {
+        enterHome();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (canJumpImmediately) {
+            jumpWhenCanClick();
+        }
+        canJumpImmediately = true;
+    }
+
+    // 跳转到主页
+    public void enterHome() {
+        startActivity(new Intent(this, MainActivity.class));
+
+        finish();
+    }
+
+}
+
+/*    private static final long DELAY = 3000;
     private static final int CODE_ENTER_HOME = 1;
 
     private Handler handler = new Handler() {
@@ -74,13 +156,4 @@ public class SplashActivity extends BaseActivity {
             }
         }).start();
 
-    }
-
-    // 跳转到主页
-    public void enterHome() {
-        startActivity(new Intent(this, MainActivity.class));
-
-        finish();
-    }
-
-}
+    }*/
