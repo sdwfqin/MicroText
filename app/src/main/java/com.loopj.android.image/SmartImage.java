ae38a7ab20a69cd201bb951d64d715ba8103f944
sdwfqin <1245366907@qq.com>
2016-07-28 15:34:50 +0800

    删除无用组件

--- app/src/main/java/com.loopj.android.image/SmartImage.java
+++
@@ -1,8 +0,0 @@
-package com.loopj.android.image;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-
-public interface SmartImage {
-    public Bitmap getBitmap(Context context);
-}
