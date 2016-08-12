c7284bc4ef94b4c504acb65b2cd3013fc8ff0c18
sdwfqin <1245366907@qq.com>
2016-08-12 21:48:18 +0800

    修复数据源

--- app/src/main/java/com/sdwfqin/microtext/base/SwipeBackActivity.java
+++ app/src/main/java/com/sdwfqin/microtext/base/SwipeBackActivity.java
@@ -13,36 +13,32 @@ import com.sdwfqin.microtext.view.SwipeBackLayout;
  * 只需要调用SwipeBackLayout的setViewPager()方法即可
  *
  * @author xiaanming
- *
  */
 public class SwipeBackActivity extends BaseActivity {
-	protected SwipeBackLayout layout;
+    protected SwipeBackLayout layout;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		layout = (SwipeBackLayout) LayoutInflater.from(this).inflate(
-				R.layout.base, null);
-		layout.attachToActivity(this);
-	}
-
-
-	@Override
-	public void startActivity(Intent intent) {
-		super.startActivity(intent);
-		overridePendingTransition(R.anim.base_slide_right_in, R.anim.base_slide_remain);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        layout = (SwipeBackLayout) LayoutInflater.from(this).inflate(
+                R.layout.base, null);
+        layout.attachToActivity(this);
+    }
 
 
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransition(R.anim.base_slide_right_in, R.anim.base_slide_remain);
+    }
 
 
-	// Press the back button in mobile phone
-	@Override
-	public void onBackPressed() {
-		super.onBackPressed();
-		overridePendingTransition(0, R.anim.base_slide_right_out);
-	}
-
+    // Press the back button in mobile phone
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0, R.anim.base_slide_right_out);
+    }
 
 
 }
