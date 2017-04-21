866dbc833bfe125e47331baf9655517b484223d8
sdwfqin <1245366907@qq.com>
2017-04-21 20:09:09 +0800

    数据源

--- App/src/main/java/com/sdwfqin/microtext/utils/AppConfig.java
+++ App/src/main/java/com/sdwfqin/microtext/utils/AppConfig.java
@@ -10,11 +10,11 @@ public class AppConfig {
     public final static String sHomeUrl = "http://www.lookmw.cn";
 
     public final static String[] sHomeTitleList = new String[]{"原创美文", "经典文章",
-            "情感", "情殇", "生活随笔", "感人爱情", "人生哲理", "长篇故事"};
+            "情感", "情殇", "心情随笔", "感人爱情", "人生哲理", "长篇故事"};
 
-    public final static String[] sHomeCode = new String[]{"/yc/list_64477_", "/jdmw/list_6_",
-            "/qingganmeiwen/list_64480_", "/shanggan/list_64482_", "/suibi/list_64483_",
-            "/ganrenwenzhang/list_64484_", "/renshengzheli/list_64486_", "/story/list_28_"};
+    public final static String[] sHomeCode = new String[]{"/yc/list_64477_", "/jdmw/?p=",
+            "/qingganmeiwen/list_64480_", "/shanggan/list_64482_", "/xinqing/?p=",
+            "/ganrenwenzhang/list_64484_", "/renshengzheli/list_64486_", "/story/?p="};
 
     public static final String COLOR = "color";
 }
