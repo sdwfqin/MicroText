67f781a485477a07357ac12b1706977d288138cc
sdwfqin <1245366907@qq.com>
2017-07-28 13:56:27 +0800

    启动页优化

--- app/src/main/java/com/sdwfqin/microtext/ui/main/MainActivity.java
+++ app/src/main/java/com/sdwfqin/microtext/ui/main/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
